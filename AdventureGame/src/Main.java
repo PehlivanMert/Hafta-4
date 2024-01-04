bb867eb5e907884b10e433bf45cb816e73542b75
PehlivanMert <pehlivannmert@gmail.com>
2024-01-04 16:52:31 +0300

    Adventure Game

--- AdventureGame/src/Main.java
+++ AdventureGame/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Oyunların bitme koşulu tüm ödüllerin toplanması bölgelere girilebiliyor ama ödül tekrar alınamıyor.
         Game game = new Game();
         game.start();
     }
