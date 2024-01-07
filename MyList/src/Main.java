f95a5f82ce99fb2b8c793f46098ead619117e727
PehlivanMert <pehlivannmert@gmail.com>
2024-01-07 16:56:49 +0300

    Update 1

--- MyList/src/Main.java
+++ MyList/src/Main.java
@@ -20,6 +20,54 @@ public class Main {
         System.out.println("Dizideki Eleman Sayısı : " + list.size());
         System.out.println("Dizinin Kapasitesi : " + list.getCapacity());
         list.toArray();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("2. indisteki değer : " + list.get(2));
+        list.remove(2);
+        list.add(40);
+        list.set(0, 100);
+        System.out.println("2. indisteki değer : " + list.get(2));
+        System.out.println(list.toString());
+
+
+        System.out.println("Liste Durumu : " + (list.isEmpty() ? "Boş" : "Dolu"));
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(20);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+
+        System.out.println("Liste Durumu : " + (list.isEmpty() ? "Boş" : "Dolu"));
+
+        // Bulduğu ilk indeksi verir
+        System.out.println("Indeks : " + list.indexOf(20));
+
+        // Bulamazsa -1 döndürür
+        System.out.println("Indeks :" + list.indexOf(100));
+
+        // Bulduğu son indeksi verir
+        System.out.println("Indeks : " + list.lastIndexOf(20));
+
+        // Listeyi Object[] dizisi olarak geri verir.
+        Object[] dizi = list.toArray();
+        System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
+
+        // Liste veri türünde alt bir list oluşturdu
+        MyList<Integer> altListem = list.sublist(0, 3);
+        System.out.println(altListem.toString());
+
+        // Değerim listedeki olup olmadığını sorguladı
+        System.out.println("Listemde 20 değeri : " + list.contains(20));
+        System.out.println("Listemde 120 değeri : " + list.contains(120));
+
+        // Listeyi tamamen boşaltır ve varsayılan boyutuna çevirir
+        list.clear();
+        System.out.println(list.toString());
     }
 }
 
